9b900bba7165d1bf73a13198930741e0d9d27e6f
Anna Georgiadou <77627644+Annageorg@users.noreply.github.com>
2021-02-19 16:40:52 +0000

    Almost final

--- index.js
+++ index.js
@@ -13,7 +13,6 @@
     document.getElementById(id).style.display = "inline-block";
   }
   
-  
   /**
    * calculates and displays the results
    */
@@ -23,6 +22,7 @@
     for (const rb of radios1) {
         if (rb.checked) {
             q1Value = rb.value;
+            console.log(q1Value);
             break;
         }
     }
@@ -32,6 +32,7 @@
     for (const rb of radios2) {
         if (rb.checked) {
             q2Value = rb.value;
+            console.log(q2Value);
             break;
         }
     }
@@ -41,6 +42,7 @@
     for (const rb of radios3) {
         if (rb.checked) {
             q3Value = rb.value;
+            console.log(q3Value);
             break;
         }
     }
@@ -49,6 +51,7 @@
     for (const rb of radios4) {
         if (rb.checked) {
             q4Value = rb.value;
+            console.log(q4Value);
             break;
         }
     }
@@ -57,23 +60,35 @@
     for (const rb of radios5) {
         if (rb.checked) {
             q5Value = rb.value;
+            console.log(q5Value);
             break;
         }
     }
 
     let total = Number(q1Value) + Number(q2Value)+ Number(q3Value)+ Number(q4Value)+ Number(q5Value);
+    console.log(total)
     let result = total / 5;
+    console.log(result)
     
-    if (result > 3.5) {
-      msg = "You answered mostly d";
+    if (result >= 3.5) {
+      msg = "KIRBY";
+      document.getElementById('kirby').style.display = "inline-block";
+      console.log(msg)
     } else if (result >= 2.5) {
-      msg = "You answered mostly c";
+      msg = "SONIC";
+      document.getElementById('sonic').style.display = "inline-block";
+      console.log(msg)
     } else if (result >= 1.5) {
-      msg = "You answered mostly b"; 
+      msg = "LINK";
+      document.getElementById('link').style.display = "inline-block"; 
+      console.log(msg)
     }else {
-      msg = "You answered mostly a";
+      msg = "MARIO";
+      document.getElementById('mario').style.display = "inline-block";
+      console.log(msg)
     }
   
-    document.getElementById("final").innerHTML = msg
+    document.getElementById('final').innerHTML = msg
   
   }
+
